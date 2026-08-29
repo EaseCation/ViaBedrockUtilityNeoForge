11c7ce66ef9e7864888e2100fb95a8f5cb7bed0a
bb-dev <admin@easecation.net>
2026-08-30 06:43:05 +0800

    Test alpha-test attachable render culling

---
+++ src/test/java/org/oryxel/viabedrockutility/attachable/AttachableRenderTypesTest.java
@@ -0,0 +1,25 @@
+package org.oryxel.viabedrockutility.attachable;
+
+import net.easecation.bedrockmotion.render.RenderControllerEvaluator;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.resources.ResourceLocation;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+class AttachableRenderTypesTest {
+    @Test
+    void bedrockAlphaTestUsesNoCullForReversedTextureMeshes() {
+        final ResourceLocation texture = ResourceLocation.parse("minecraft:textures/items/wood_sword");
+        final RenderControllerEvaluator.EvaluatedRenderPass pass =
+                new RenderControllerEvaluator.EvaluatedRenderPass(
+                        "default", null, "geometry.sword", texture.toString(),
+                        Map.of(), Map.of(), false, true,
+                        RenderControllerEvaluator.BlendMode.OPAQUE, false, false, 1.0F, 0xFFFFFFFF);
+
+        assertSame(RenderType.entityCutoutNoCull(texture),
+                AttachableRenderTypes.renderType("entity_alphatest", pass, texture));
+    }
+}
